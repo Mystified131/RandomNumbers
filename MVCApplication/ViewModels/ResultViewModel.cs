3e2ea94603044c203ec2baa30e88ed7d423ae1ba
mystified131 <mystifiedthomas@gmail.com>
2018-12-26 06:53:50 -0600

    Initial commit.

---
+++ MVCApplication/ViewModels/ResultViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.ViewModels
+{
+
+    public class ResultViewModel
+    {
+        [Required]
+        public int quant { get; set; }
+        public List<string> RandomNumbers { get; set; }
+    }
+}
+
+
